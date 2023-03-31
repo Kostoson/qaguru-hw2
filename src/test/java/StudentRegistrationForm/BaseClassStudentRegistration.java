8cdea9013ccaab638e0a4303d3c3aeb175450cb1
Константин <kostos1995@mail.ru>
2023-03-31 12:02:04 +0300

    test3

--- src/test/java/StudentRegistrationForm/BaseClassStudentRegistration.java
+++ src/test/java/StudentRegistrationForm/BaseClassStudentRegistration.java
@@ -1,2 +1,18 @@
-package StudentRegistrationForm;public class BaseClassStudentRegistration {
+package StudentRegistrationForm;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+
+
+
+public class BaseClassStudentRegistration {
+    @BeforeAll
+    static void beforeAll(){
+
+        Configuration.baseUrl = "https://demoqa.com";
+        Configuration.browserSize = "1920x1080";
+        Configuration.browserPosition = "0x0";
+    }
+
+
 }
--- src/test/java/StudentRegistrationForm/PositiveRegistrationTest.java
+++ src/test/java/StudentRegistrationForm/PositiveRegistrationTest.java
@@ -1,2 +1,50 @@
-package StudentRegistrationForm;public class PositiveRegistrationTest {
+package StudentRegistrationForm;
+
+import com.codeborne.selenide.selector.ByText;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+import static org.openqa.selenium.Keys.CONTROL;
+public class PositiveRegistrationTest extends BaseClassStudentRegistration{
+
+@Test
+    void positiveFullRegistration(){
+    open("/automation-practice-form");
+    executeJavaScript("$('#fixedban').remove()");
+    executeJavaScript("$('footer').remove()");
+    $("input#firstName").setValue("Kostos");
+    $("input#lastName").setValue("Dubinin");
+    $("input#userEmail").setValue("name@test.ru");
+    $("#genterWrapper").$(byText("Male")).click();
+    $("input#userNumber").setValue("7999123445");
+    $("#dateOfBirthInput").sendKeys(CONTROL + "A");
+    $("#dateOfBirthInput").sendKeys("01.01.1995");
+    $("#dateOfBirthInput").pressEnter();
+    $("#subjectsContainer input").setValue("Maths").pressEnter();
+    $("#hobbiesWrapper").$(byText("Sport")).click();
+    $("#uploadPicture").uploadFromClasspath("images/im.png");
+    $("#currentAddress").setValue("Test");
+    $("#state").click();
+    $("#stateCity-wrapper").$(byText("NCR")).click();
+    $("#city").click();
+    $("#stateCity-wrapper").$(byText("Delhi")).click();
+    $("#submit").click();
+
+    $("#table-responsive").shouldHave(text("Kostos Dubinin"),
+            text("name@test.ru"),
+            text("Kostos Dubinin"),
+            text("Male"),
+            text("7999123445"),
+            text("01 January,1995"),
+            text("Maths"),
+            text("Sports"),
+            text("im.png"),
+            text("Test"),
+            text("NCR Delhi")
+    );
+
+
+}
 }
