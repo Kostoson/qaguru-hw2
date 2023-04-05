7c420dade3df3b239f81ea6065c74796fe344a0f
Konstantin Dubinin <kostos1995@mail.ru>
2023-04-05 19:42:36 +0300

    home work

--- src/test/java/someTestInGitHub/TestInGitHub.java
+++ src/test/java/someTestInGitHub/TestInGitHub.java
@@ -1,2 +1,31 @@
-package someTestInGitHub;public class TestInGitHub {
+package someTestInGitHub;
+
+import com.codeborne.selenide.Configuration;
+
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+
+import static com.codeborne.selenide.Selenide.*;
+
+public class TestInGitHub {
+
+    @Test
+    void sampleCodeTest() {
+
+        Configuration.browserSize = "1920x1080";
+        // Открыть страницу Selenide в Github
+        open("https://github.com/selenide/selenide");
+        // Перейти в раздел Wiki проекта
+        $("a#wiki-tab").click();
+        // Убедится, что в списке страниц (Pages) есть страница SoftAssertions
+        $(".Layout-main").$("ul").$("li").sibling(5).shouldHave(text("Soft assertions"));
+        // Открыть страницу SoftAssertions
+        $(".Layout-main").$("ul").$("li").sibling(5).$("a").click();
+        // Проверьте что внутри есть пример кода для JUnit5
+        $("div.Layout-main div.markdown-body h4").sibling(5).
+                shouldHave(text("3. Using JUnit5 extend test class:"));
+
+    }
+
 }
--- src/test/java/studentRegistrationForm/BaseClassStudentRegistration.java
+++ src/test/java/studentRegistrationForm/BaseClassStudentRegistration.java
@@ -4,11 +4,10 @@ import com.codeborne.selenide.Configuration;
 import org.junit.jupiter.api.BeforeAll;
 
 
-
 public class BaseClassStudentRegistration {
     @BeforeAll
 
-    static void beforeAlll(){
+    static void beforeAll() {
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
