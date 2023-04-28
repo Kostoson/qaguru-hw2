cce2e19ce9f3ea6aa72abc6d3c0059a76b4ca7c0
Konstantin Dubinin <kostos1995@mail.ru>
2023-04-28 11:50:32 +0300

    fix

--- src/test/java/parameterizetests/yandexgames/pages/SearchResultsOnDemandPage.java
+++ src/test/java/parameterizetests/yandexgames/pages/SearchResultsOnDemandPage.java
@@ -7,9 +7,16 @@ import static com.codeborne.selenide.Selenide.$;
 
 public class SearchResultsOnDemandPage {
     SelenideElement secondResult = $("ul.grid-list_size_adaptive_recommended_new").$("li.grid-list__game-item ").sibling(0).$("span.game-card__title");
+    SelenideElement headerResultsPage = $("span.section-header__text");
 
     public void checkSecondResult(String value) {
         secondResult.shouldHave(text(value));
     }
+
+
+    public void checkHeaderResultsPage(String value) {
+        headerResultsPage.shouldHave(text(value));
+    }
+
 
 }
